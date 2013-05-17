bf4453eb6fd5771d44eb3657152559ae49d11361
James Raspass <jraspass@gmail.com>
2013-05-17 01:09:23 +0100

    Encode has a wrapper around the C API too, same speed not worth the import

--- bench/decode_utf8.pl
+++ bench/decode_utf8.pl
@@ -4,11 +4,18 @@ use strict;
 use warnings;
 
 use Benchmark 'cmpthese';
-use Encode qw/decode decode_utf8/;
+use Encode qw/decode decode_utf8 _utf8_on/;
 
 cmpthese -1, {
     decode         => sub { decode 'UTF-8', '☃' },
     decode_utf8    => sub { decode_utf8 '☃' },
+    _utf8_on       => sub {
+        my $bytes = '☃';
+
+        _utf8_on $bytes;
+
+        return $bytes;
+    },
     'utf8::decode' => sub {
         my $bytes = '☃';
 

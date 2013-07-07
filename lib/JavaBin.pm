40564c4fd855cbc5adb39e69d1937f643edd4687
James Raspass <jraspass@gmail.com>
2013-07-07 19:32:45 +0100

    added POD

--- lib/JavaBin.pm
+++ lib/JavaBin.pm
@@ -144,3 +144,67 @@ sub _vint {
 }
 
 1;
+
+__END__
+
+=head1 NAME
+
+JavaBin - Apache Solr JavaBin (de)serializer
+
+=head1 SYNOPSIS
+
+ use JavaBin;
+
+ my $result = from_javabin $binary_data;
+
+=head1 DESCRIPTION
+
+JavaBin is a compact binary format used by L<Apache Solr|http://lucene.apache.org/solr>.
+
+For more information on this format see the L<Solr Wiki|http://wiki.apache.org/solr/javabin>.
+
+This package provides a deserializer for this format, with a serializer planned.
+
+=head1 FUNCTIONS
+
+=head2 from_javabin
+
+ my $result = from_javabin $binary_data;
+
+Accepts one argument, a binary string of containing the JavaBin.
+
+Returns a scalar representation of the data, be that undef, number, string, or reference.
+
+This function does no error checking, hand it invalid JavaBin and it will probably die.
+
+=head1 CAVEATS
+
+Due to the differences between Java and Perl not all data structures can be mapped one-to-one.
+
+An example of such mapping is a Java interator whcih becomes a Perl array during deserialization.
+Additionally a Java HashMap, Named List, or Ordered Map will become a Perl hash.
+
+=head1 TODO
+
+=over 2
+
+=item * C<to_javabin> serializer.
+
+=item * XS implementation.
+
+=back
+
+=head1 INSPIRATION
+
+This package was inspired by the L<Ruby JavaBin library|https://github.com/kennyj/java_bin>.
+Both that library and the Java JavaBin library proved very helpful in understanding JavaBin.
+
+=head1 AUTHOR
+
+James Raspass E<lt>jraspass@gmail.comE<gt>
+
+=head1 COPYRIGHT AND LICENSE
+
+Copyright 2013 by James Raspass
+
+This library is free software; you can redistribute it and/or modify it under the same terms as Perl itself.

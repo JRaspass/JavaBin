8e7db59d473f77539191294278364a135994a943
James Raspass <jraspass@gmail.com>
2013-12-27 09:51:44 +0000

    fix java warnings

--- t/MakeData.java
+++ t/MakeData.java
@@ -10,38 +10,38 @@ public class MakeData {
             new JavaBinCodec().marshal(
                 new SolrDocumentList(){{
                     add(new SolrDocument(){{
-                        addField("byte_arr", new byte[]{-128, 0, 127});
-                        addField("byte_max", (byte)127);
-                        addField("byte_min", (byte)-128);
-                        addField("byte_zero", (byte)0);
-                        addField("enum_max", new EnumFieldValue(2147483647, "max"));
-                        addField("enum_min", new EnumFieldValue(-2147483648, "min"));
-                        addField("enum_snowman", new EnumFieldValue(123, "☃"));
-                        addField("enum_zero", new EnumFieldValue(0, "zero"));
-                        addField("false", false);
-                        addField("hash_map", new HashMap<String, Object>(){{
+                        put("byte_arr", new byte[]{-128, 0, 127});
+                        put("byte_max", (byte)127);
+                        put("byte_min", (byte)-128);
+                        put("byte_zero", (byte)0);
+                        put("enum_max", new EnumFieldValue(2147483647, "max"));
+                        put("enum_min", new EnumFieldValue(-2147483648, "min"));
+                        put("enum_snowman", new EnumFieldValue(123, "☃"));
+                        put("enum_zero", new EnumFieldValue(0, "zero"));
+                        put("false", false);
+                        put("hash_map", new HashMap<String, Object>(){{
                             put("foo", "bar");
                             put("baz", "qux");
                         }});
-                        addField("iterator", Arrays.asList(arr).iterator());
-                        addField("named_list", new NamedList(){{
+                        put("iterator", Arrays.asList(arr).iterator());
+                        put("named_list", new NamedList<String>(){{
                             add("foo", "bar");
                             add("baz", "qux");
                         }});
-                        addField("null", null);
-                        addField("one_small_step", new Date(-14159040000L));
-                        addField("pangram", "The quick brown fox jumped over the lazy dog");
-                        addField("pi_double", 3.14159265358979);
-                        addField("pi_float", 3.14159f);
-                        addField("short_max", (short)32_767);
-                        addField("short_min", (short)-32_768);
-                        addField("simple_ordered_map", new SimpleOrderedMap(){{
+                        put("null", null);
+                        put("one_small_step", new Date(-14159040000L));
+                        put("pangram", "The quick brown fox jumped over the lazy dog");
+                        put("pi_double", 3.14159265358979);
+                        put("pi_float", 3.14159f);
+                        put("short_max", (short)32_767);
+                        put("short_min", (short)-32_768);
+                        put("simple_ordered_map", new SimpleOrderedMap<String>(){{
                             add("foo", "bar");
                             add("baz", "qux");
                         }});
-                        addField("snowman", "☃");
-                        addField("str_arr", arr);
-                        addField("true", true);
+                        put("snowman", "☃");
+                        put("str_arr", arr);
+                        put("true", true);
                     }});
                     setMaxScore(0.1f);
                     setNumFound(2);

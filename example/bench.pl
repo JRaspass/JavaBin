c66bbf9d6d67cd5bc868d02de152c8ed1236b0df
James Raspass <jraspass@gmail.com>
2014-01-01 17:21:53 +0000

    use aliases in bench.pl to prevent very long output lines

--- example/bench.pl
+++ example/bench.pl
@@ -55,41 +55,58 @@ my $mpack  = Data::MessagePack->new;
 my $sereal = Sereal::Encoder->new;
 
 my %alts; %alts = (
-    'CBOR::XS' => {
-        from => sub { decode_cbor $alts{'CBOR::XS'}{data} },
+    CBOR => {
+        from => sub { decode_cbor $alts{CBOR}{data} },
+        pkg  => 'CBOR::XS',
         to   => sub { encode_cbor $languages },
     },
-    'Data::Dumper' => {
-        from => sub { eval $alts{'Data::Dumper'}{data} },
+    Dump => {
+        from => sub { eval $alts{Dump}{data} },
+        pkg  => 'Data::Dumper',
         to   => sub { Dumper $languages },
     },
-    'Data::MessagePack' => {
-        from => sub { $mpack->unpack($alts{'Data::MessagePack'}{data}) },
-        to   => sub { $mpack->pack($languages) },
-    },
-    JavaBin => {
-        from => sub { from_javabin $alts{JavaBin}{data} },
+    Java => {
+        from => sub { from_javabin $alts{Java}{data} },
+        pkg  => 'JavaBin',
         to   => sub {   to_javabin $languages },
     },
-    'JSON::XS' => {
-        from => sub { decode_json $alts{'JSON::XS'}{data} },
+    JSON => {
+        from => sub { decode_json $alts{JSON}{data} },
+        pkg  => 'JSON::XS',
         to   => sub { encode_json $languages },
     },
-    Sereal => {
-        from => sub { $sereal->decode($alts{Sereal}{data}) },
+    MsgP => {
+        from => sub { $mpack->unpack($alts{MsgP}{data}) },
+        pkg  => 'Data::MessagePack',
+        to   => sub { $mpack->pack($languages) },
+    },
+    Srel => {
+        from => sub { $sereal->decode($alts{Srel}{data}) },
+        pkg  => 'Sereal',
         to   => sub { $sereal->encode($languages) },
     },
-    Storable => {
-        from => sub {   thaw $alts{Storable}{data} },
+    Stor => {
+        from => sub {   thaw $alts{Stor}{data} },
+        pkg  => 'Storable',
         to   => sub { freeze $languages },
     },
-    'YAML::XS' => {
-        from => sub { Load $alts{'YAML::XS'}{data} },
+    YAML => {
+        from => sub { Load $alts{YAML}{data} },
+        pkg  => 'YAML::XS',
         to   => sub { Dump $languages },
     },
 );
 
-print "Encode\n";
+print "Modules\n\n";
+
+{
+    no strict 'refs';
+
+    printf "%-5s%-18s%s\n", $_, $alts{$_}{pkg}, ${"$alts{$_}{pkg}::VERSION"}
+        for sort keys %alts;
+}
+
+print "\nEncode\n";
 
 cmpthese -1, { map { $_ => $alts{$_}{to} } keys %alts };
 
@@ -97,7 +114,7 @@ print "\nSize\n\n";
 
 $_->{size} = length( $_->{data} = $_->{to}->() ) for values %alts;
 
-printf "%-18s%d bytes\n", $_, $alts{$_}{size}
+printf "%-5s%d bytes\n", $_, $alts{$_}{size}
     for sort { $alts{$b}{size} <=> $alts{$a}{size} } keys %alts;
 
 print "\nDecode\n";

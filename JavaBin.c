39f1e9c7c8b23812336e8d9e7dff4af32f187262
James Raspass <jraspass@gmail.com>
2015-09-15 03:03:11 +0100

    Simplify code with macros

--- JavaBin.c
+++ JavaBin.c
@@ -3,6 +3,12 @@
 #include <perl.h>
 #include <stdint.h>
 
+// This layer of indirection allows us to call macros with macro arguments.
+#define CALL(a,...) a(__VA_ARGS__)
+
+#define hv_set(hv, key, val, hash) \
+    Perl_hv_common(aTHX_ hv, NULL, STR_WITH_LEN(key), 0, HV_FETCH_ISSTORE, val, hash)
+
 #define READ_LEN (in[-1] & 31) == 31 ? 31 + read_v_int() : in[-1] & 31
 
 typedef union { uint64_t i; double d; } int_double;
@@ -88,7 +94,7 @@ static SV* read_sv(pTHX) {
 read_undef:
     return &PL_sv_undef;
 read_bool: {
-        SV *rv = Perl_newSV_type(aTHX_ SVt_IV), *sv = in[-1] == 1 ? bool_true : bool_false;
+        SV *rv = newSV_type(SVt_IV), *sv = in[-1] == 1 ? bool_true : bool_false;
 
         SvREFCNT(sv)++;
         SvROK_on(rv);
@@ -97,13 +103,13 @@ read_bool: {
         return rv;
     }
 read_byte:
-    return Perl_newSViv(aTHX_ (int8_t) *in++);
+    return newSViv((int8_t) *in++);
 read_short: {
         const int16_t s = in[0] << 8 | in[1];
 
         in += 2;
 
-        return Perl_newSViv(aTHX_ s);
+        return newSViv(s);
     }
 read_double: {
         // For perls with double length NVs this conversion is simple.
@@ -126,9 +132,9 @@ read_double: {
 
         sprintf(str, "%.14f", u.d);
 
-        return Perl_newSVnv(aTHX_ strtold(str, NULL));
+        return newSVnv(strtold(str, NULL));
     #else
-        return Perl_newSVnv(aTHX_ u.d);
+        return newSVnv(u.d);
     #endif
     }
 read_int: {
@@ -136,7 +142,7 @@ read_int: {
 
         in += 4;
 
-        return Perl_newSViv(aTHX_ i);
+        return newSViv(i);
     }
 read_long: {
         const int64_t l = (uint64_t) in[0] << 56 |
@@ -150,7 +156,7 @@ read_long: {
 
         in += 8;
 
-        return Perl_newSViv(aTHX_ l);
+        return newSViv(l);
     }
 read_float: {
         // JavaBin has a 4byte float format, NVs in perl are double or long double,
@@ -166,9 +172,9 @@ read_float: {
         sprintf(str, "%f", u.f);
 
     #ifdef USE_LONG_DOUBLE
-        return Perl_newSVnv(aTHX_ strtold(str, NULL));
+        return newSVnv(strtold(str, NULL));
     #else
-        return Perl_newSVnv(aTHX_ strtod(str, NULL));
+        return newSVnv(strtod(str, NULL));
     #endif
     }
 read_date: {
@@ -197,12 +203,12 @@ read_date: {
                 t->tm_sec,
                 (uint32_t) (date_ms % 1000));
 
-        return Perl_newSVpvn(aTHX_ date_str, 24);
+        return newSVpvn(date_str, 24);
     }
 read_solr_doc:
     in++;     // Assume a solr doc is a map.
 read_map: {
-        HV *hv = (HV*)Perl_newSV_type(aTHX_ SVt_PVHV);
+        HV *hv = (HV*)newSV_type(SVt_PVHV);
 
         uint32_t len = in[-1] >> 5 ? READ_LEN : read_v_int();
 
@@ -233,10 +239,10 @@ read_map: {
                 }
             }
 
-            Perl_hv_common(aTHX_ hv, NULL, key.key, key.len, key.flags, HV_FETCH_ISSTORE, read_sv(aTHX), 0);
+            hv_common(hv, NULL, key.key, key.len, key.flags, HV_FETCH_ISSTORE, read_sv(aTHX), 0);
         }
 
-        SV *rv = Perl_newSV_type(aTHX_ SVt_IV);
+        SV *rv = newSV_type(SVt_IV);
 
         SvROK_on(rv);
         SvRV_set(rv, (SV*)hv);
@@ -244,20 +250,17 @@ read_map: {
         return rv;
     }
 read_solr_doc_list: {
-        HV *hv = (HV*)Perl_newSV_type(aTHX_ SVt_PVHV);
+        HV *hv = (HV*)newSV_type(SVt_PVHV);
 
         // Assume values are in an array, skip tag & read_sv.
         in++;
 
-        Perl_hv_common(aTHX_ hv, NULL, "numFound", 8, 0, HV_FETCH_ISSTORE, read_sv(aTHX), numFound);
+        hv_set(hv, "numFound", read_sv(aTHX), numFound);
+        hv_set(hv, "start",    read_sv(aTHX), start);
+        hv_set(hv, "maxScore", read_sv(aTHX), maxScore);
+        hv_set(hv, "docs",     read_sv(aTHX), docs);
 
-        Perl_hv_common(aTHX_ hv, NULL, "start", 5, 0, HV_FETCH_ISSTORE, read_sv(aTHX), start);
-
-        Perl_hv_common(aTHX_ hv, NULL, "maxScore", 8, 0, HV_FETCH_ISSTORE, read_sv(aTHX), maxScore);
-
-        Perl_hv_common(aTHX_ hv, NULL, "docs", 4, 0, HV_FETCH_ISSTORE, read_sv(aTHX), docs);
-
-        SV *rv = Perl_newSV_type(aTHX_ SVt_IV);
+        SV *rv = newSV_type(SVt_IV);
 
         SvROK_on(rv);
         SvRV_set(rv, (SV*)hv);
@@ -265,7 +268,7 @@ read_solr_doc_list: {
         return rv;
     }
 read_byte_array: {
-        AV *av = (AV*)Perl_newSV_type(aTHX_ SVt_PVAV);
+        AV *av = (AV*)newSV_type(SVt_PVAV);
 
         SSize_t len = read_v_int();
 
@@ -275,9 +278,9 @@ read_byte_array: {
         AvFILLp(av) = AvMAX(av) = len - 1;
 
         while (len--)
-            *ary++ = Perl_newSViv(aTHX_ (int8_t) *in++);
+            *ary++ = newSViv((int8_t) *in++);
 
-        SV *rv = Perl_newSV_type(aTHX_ SVt_IV);
+        SV *rv = newSV_type(SVt_IV);
 
         SvROK_on(rv);
         SvRV_set(rv, (SV*)av);
@@ -285,16 +288,16 @@ read_byte_array: {
         return rv;
     }
 read_iterator: {
-        AV *av = (AV*)Perl_newSV_type(aTHX_ SVt_PVAV);
+        AV *av = (AV*)newSV_type(SVt_PVAV);
 
         uint32_t len = 0;
 
         while (*in != 15)
-            Perl_av_store(aTHX_ av, len++, read_sv(aTHX));
+            av_store(av, len++, read_sv(aTHX));
 
         in++;
 
-        SV *rv = Perl_newSV_type(aTHX_ SVt_IV);
+        SV *rv = newSV_type(SVt_IV);
 
         SvROK_on(rv);
         SvRV_set(rv, (SV*)av);
@@ -304,13 +307,13 @@ read_iterator: {
 read_enum: {
         SV *sv = read_sv(aTHX); // small_int if +ve, int otherwise.
 
-        Perl_sv_upgrade(aTHX_ sv, SVt_PVMG);
+        sv_upgrade(sv, SVt_PVMG);
 
         in++;
 
         const STRLEN len = READ_LEN;
 
-        char *str = Perl_sv_grow(aTHX_ sv, len + 1);
+        char *str = sv_grow(sv, len + 1);
 
         memcpy(str, in, len);
 
@@ -322,12 +325,12 @@ read_enum: {
 
         SvFLAGS(sv) = SVf_IOK | SVp_IOK | SVs_OBJECT | SVf_POK | SVp_POK | SVt_PVMG | SVf_UTF8;
 
-        HV *stash = Perl_gv_stashpvn(aTHX_ STR_WITH_LEN("JavaBin::Enum"), 0);
+        HV *stash = CALL(gv_stashpvn, STR_WITH_LEN("JavaBin::Enum"), 0);
 
         SvREFCNT(stash)++;
         SvSTASH_set(sv, stash);
 
-        SV *rv = Perl_newSV_type(aTHX_ SVt_IV);
+        SV *rv = newSV_type(SVt_IV);
 
         SvROK_on(rv);
         SvRV_set(rv, sv);
@@ -337,7 +340,7 @@ read_enum: {
 read_string: {
         const STRLEN len = READ_LEN;
 
-        SV *sv = Perl_newSV_type(aTHX_ SVt_PV);
+        SV *sv = newSV_type(SVt_PV);
 
         char *str = SvPVX(sv) = (char*)safemalloc(len);
 
@@ -356,7 +359,7 @@ read_small_int: {
         if (in[-1] & 16)
             result |= read_v_int() << 4;
 
-        return Perl_newSViv(aTHX_ result);
+        return newSViv(result);
     }
 read_small_long: {
         uint64_t result = in[-1] & 15;
@@ -369,10 +372,10 @@ read_small_long: {
             while (in[-1] & 128 && (shift += 7));
         }
 
-        return Perl_newSViv(aTHX_ result);
+        return newSViv(result);
     }
 read_array: {
-        AV *av = (AV*)Perl_newSV_type(aTHX_ SVt_PVAV);
+        AV *av = (AV*)newSV_type(SVt_PVAV);
 
         SSize_t len = READ_LEN;
 
@@ -384,7 +387,7 @@ read_array: {
         while (len--)
             *ary++ = read_sv(aTHX);
 
-        SV *rv = Perl_newSV_type(aTHX_ SVt_IV);
+        SV *rv = newSV_type(SVt_IV);
 
         SvROK_on(rv);
         SvRV(rv) = (SV*)av;
@@ -398,7 +401,7 @@ static void grow_out(pTHX_ const STRLEN want) {
 
     // If we want more than we have, realloc the string.
     if (len + want >= SvLEN(out_sv)) {
-        Perl_sv_grow(aTHX_ out_sv, len + want);
+        sv_grow(out_sv, len + want);
 
         out_buf = (uint8_t *)SvPVX(out_sv) + len;
     }
@@ -591,14 +594,14 @@ static void from_javabin(pTHX_ CV *cv __attribute__((unused))) {
     cache_pos = 0;
 
     if (SvCUR(*sp) < 2)
-        Perl_croak(aTHX_ "Invalid from_javabin input: insufficient length");
+        croak("Invalid from_javabin input: insufficient length");
 
     in = (uint8_t *) SvPVX(*sp);
 
     if (*in++ != 2)
-        Perl_croak(aTHX_ "Invalid from_javabin input: expected version 2");
+        croak("Invalid from_javabin input: expected version 2");
 
-    *sp = Perl_sv_2mortal(aTHX_ read_sv(aTHX));
+    *sp = sv_2mortal(read_sv(aTHX));
 
     PL_stack_sp = sp;
 }
@@ -608,7 +611,7 @@ static void to_javabin(pTHX_ CV *cv __attribute__((unused))) {
 
     out_sv = PAD_SV(PL_op->op_targ);
 
-    Perl_sv_upgrade(aTHX_ out_sv, SVt_PV);
+    sv_upgrade(out_sv, SVt_PV);
     SvPOK_on(out_sv);
 
     out_buf = (uint8_t *)SvPVX(out_sv);
@@ -633,14 +636,14 @@ static void deref(pTHX_ CV *cv __attribute__((unused))) {
 }
 
 static void sub(pTHX_ char *name, STRLEN len, XSUBADDR_t addr) {
-    CV *cv = (CV*)Perl_newSV_type(aTHX_ SVt_PVCV);
-    GV *gv = Perl_gv_fetchpvn_flags(aTHX_ name, len, GV_ADD, SVt_PVCV);
+    CV *cv = (CV*)newSV_type(SVt_PVCV);
+    GV *gv = gv_fetchpvn_flags(name, len, GV_ADD, SVt_PVCV);
 
     CvISXSUB_on(cv);
     CvXSUB(cv) = addr;
 
+    CvGV_set(cv, gv);
     GvCV_set(gv, cv);
-    Perl_cvgv_set(aTHX_ cv, gv);
 
     SvFLAGS(GvSTASH(gv)) |= SVf_AMAGIC;
 }
@@ -654,62 +657,60 @@ void boot_JavaBin(pTHX_ CV *cv __attribute__((unused))) {
     sub(aTHX_ STR_WITH_LEN("JavaBin::Enum::(0+"), deref);
     sub(aTHX_ STR_WITH_LEN("JavaBin::Enum::(\"\""), deref);
 
-    Perl_sv_setsv_flags(
-        aTHX_
-        GvSV(Perl_gv_fetchpvn_flags(aTHX_ STR_WITH_LEN("JavaBin::Bool::()"), GV_ADD, SVt_PV)),
+    sv_setsv_flags(
+        GvSV(CALL(gv_fetchpvn_flags, STR_WITH_LEN("JavaBin::Bool::()"), GV_ADD, SVt_PV)),
         &PL_sv_yes,
         0
         );
-    Perl_sv_setsv_flags(
-        aTHX_
-        GvSV(Perl_gv_fetchpvn_flags(aTHX_ STR_WITH_LEN("JavaBin::Enum::()"), GV_ADD, SVt_PV)),
+    sv_setsv_flags(
+        GvSV(CALL(gv_fetchpvn_flags, STR_WITH_LEN("JavaBin::Enum::()"), GV_ADD, SVt_PV)),
         &PL_sv_yes,
         0
         );
 
     // Make two bools (true and false), store them in globals.
-    bool_stash = Perl_gv_stashpvn(aTHX_ STR_WITH_LEN("JavaBin::Bool"), 0);
+    bool_stash = CALL(gv_stashpvn, STR_WITH_LEN("JavaBin::Bool"), 0);
     SvREFCNT(bool_stash) += 2;
 
-    bool_true  = Perl_newSViv(aTHX_ 1);
-    bool_false = Perl_newSViv(aTHX_ 0);
+    bool_true  = newSViv(1);
+    bool_false = newSViv(0);
 
-    Perl_sv_upgrade(aTHX_ bool_true,  SVt_PVMG);
-    Perl_sv_upgrade(aTHX_ bool_false, SVt_PVMG);
+    sv_upgrade(bool_true,  SVt_PVMG);
+    sv_upgrade(bool_false, SVt_PVMG);
 
     SvOBJECT_on(bool_true);
     SvOBJECT_on(bool_false);
 
-    // Perl_sv_setsv_flags will set these back to 1.
+    // sv_setsv_flags will set these back to 1.
     SvREFCNT(bool_true) = SvREFCNT(bool_false) = 0;
 
     SvSTASH(bool_true) = SvSTASH(bool_false) = bool_stash;
 
     // Take refs to the bool and store them on the JavaBin pkg.
-    SV *sv = Perl_newSV_type(aTHX_ SVt_IV);
+    SV *sv = newSV_type(SVt_IV);
 
     SvROK_on(sv);
     SvRV(sv) = bool_true;
 
-    Perl_sv_setsv_flags(
-        aTHX_
-        GvSV(Perl_gv_fetchpvn_flags(aTHX_ STR_WITH_LEN("JavaBin::true"), GV_ADD, SVt_PV)),
+    sv_setsv_flags(
+        GvSV(CALL(gv_fetchpvn_flags, STR_WITH_LEN("JavaBin::true"), GV_ADD, SVt_PV)),
         sv,
         0
         );
 
-    sv = Perl_newSV_type(aTHX_ SVt_IV);
+    sv = newSV_type(SVt_IV);
 
     SvROK_on(sv);
     SvRV(sv) = bool_false;
 
-    Perl_sv_setsv_flags(
-        aTHX_
-        GvSV(Perl_gv_fetchpvn_flags(aTHX_ STR_WITH_LEN("JavaBin::false"), GV_ADD, SVt_PV)),
+    sv_setsv_flags(
+        GvSV(CALL(gv_fetchpvn_flags, STR_WITH_LEN("JavaBin::false"), GV_ADD, SVt_PV)),
         sv,
         0
         );
 
+    // FIXME Could these be done at compile time? Or is it impossible given
+    // different hashing algos?
     // Precompute some hash keys.
     PERL_HASH(docs,     "docs",     4);
     PERL_HASH(maxScore, "maxScore", 8);
